860b9bd4e40144f232664c6465f6bb6cb5a4b1bb
Bawantha1998 <bawanthaprampd918@outlook.com>
2024-02-05 08:00:09 +0530

    Nav.jsx mobile responsive done

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -6,7 +6,7 @@ import { navLinks } from "../constants";
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
-      <nav>
+      <nav className='flex justify-between items-center max-container'>
         <a href="/">
           <img
             src={headerLogo}
@@ -28,8 +28,13 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg:block">
-          <img src={hamburger} alt="hamburger icon" width={25} height={25} />
+        <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+          <a href='/'>Sign in</a>
+          <span>/</span>
+          <a href='/'>Explore now</a>
+        </div>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
         </div>
       </nav>
     </header>
