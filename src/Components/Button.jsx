32f76dc3c9b09e1bb3e93086ce0c18d53bc95d41
Bawantha1998 <bawanthaprampd918@outlook.com>
2024-04-09 18:38:56 +0530

    Superquality.jsx

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -14,11 +14,11 @@ const Button = ({
     rounded-full text-white
     border-coral-red">
     {label}
-    <img
+    {iconURL && <img
     src={iconURL}
     alt="arrow right icon"
     className="ml-2 rounded-full w-5 h-5"
-    />
+    />}
     </button>;
 };
 
